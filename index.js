a93fbe0fa58a491a1d49454a80a602888eafa2db
Jrb9477 <jackblythe1982@gmail.com>
2025-01-18 18:15:12 -0800

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["DM me to Contact PR Support"];
+const statusMessages = ["Moderating the Operations Department"];
 const statusTypes = [ 'online'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
